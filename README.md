904e7bdae52fe96b423e54f66269ac7cf6751140
Dy <gdy1218@gmail.com>
2023-04-22 23:54:49 +0200

    project updated

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Virtual Room Tour
 
 ## A virtal room toor project using three.js
+
+* Option 1: CubMapping <br>
+
+  ```
+      cube.geometry.scale(1, 1, -1);
+  ```
+
+* Option 2: SphereMapping<br>
+
+  ```
+      sphereGeometry.scale(1, 1, -1);
+  ```
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+
 
 let scene, camera, renderer, controls;
 
@@ -11,11 +13,11 @@ function init() {
 
   // setup camera
   camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerWidth, 0.1, 100);
-  camera.position.set(0, 0, 0.01)
+  camera.position.set(0, 0, 0.1)
   //camera.position.z = 3;
 
   // renderer
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("app").appendChild(renderer.domElement);
@@ -23,27 +25,44 @@ function init() {
   // camera controls
   controls = new OrbitControls(camera, renderer.domElement)
 
+  /**
+   * Option 1: CubeMap view
+   */
+  // const materials = [];
 
-  const materials = [];
+  // // left, right, top, down, front, back
+  // const texture_px = new THREE.TextureLoader().load('/images/room/px.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_px }));
+  // const texture_nx = new THREE.TextureLoader().load('/images/room/nx.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_nx }));
+  // const texture_py = new THREE.TextureLoader().load('/images/room/py.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_py }));
+  // const texture_ny = new THREE.TextureLoader().load('/images/room/ny.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_ny }));
+  // const texture_pz = new THREE.TextureLoader().load('/images/room/pz.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_pz }));
+  // const texture_nz = new THREE.TextureLoader().load('/images/room/nz.png');
+  // materials.push(new THREE.MeshBasicMaterial({ map: texture_nz }));
 
-  // left, right, top, down, front, back
-  const texture_px = new THREE.TextureLoader().load('/images/room/px.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_px }));
-  const texture_nx = new THREE.TextureLoader().load('/images/room/nx.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_nx }));
-  const texture_py = new THREE.TextureLoader().load('/images/room/py.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_py }));
-  const texture_ny = new THREE.TextureLoader().load('/images/room/ny.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_ny }));
-  const texture_pz = new THREE.TextureLoader().load('/images/room/pz.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_pz }));
-  const texture_nz = new THREE.TextureLoader().load('/images/room/nz.png');
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_nz }));
+
+  // const cube = new THREE.Mesh(new THREE.BoxGeometry( 10, 10, 10 ), materials);
+  // cube.geometry.scale(1, 1, -1);
+  // scene.add(cube);
+
+  /**
+   * Option 2: 3D View - SphereMapping
+   */
+  const sphereGeometry = new THREE.SphereGeometry(5, 30, 30);
+ 
+  const loader = new RGBELoader();
+  loader.load('/images/room/studio.hdr', (texture) => {
+    const sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    scene.add(sphere);
+  })
   
+  sphereGeometry.scale(1, 1, -1);
   
-  const cube = new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ), materials);
-  cube.geometry.scale(1, 1, -1);
-  scene.add(cube);
 
   window.addEventListener('resize', onWindowResize)
 
